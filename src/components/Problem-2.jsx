b44cad4cef5f397281b5b307388e387c4814503b
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-09-24 03:32:16 +0600

    Problem 2 done api not working

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Problem2 = () => {
+const App = () => {
+  const [modalAVisible, setModalAVisible] = useState(false);
+  const [modalBVisible, setModalBVisible] = useState(false);
+  const [modalCVisible, setModalCVisible] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [onlyEven, setOnlyEven] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [modalContact, setModalContact] = useState(null);
+  const [page, setPage] = useState(1);
+  const pageSize = 10;
+  const apiBaseUrl = "https://contact.mediusware.com/api-doc";
 
-    return (
+  useEffect(() => {
+    if (modalAVisible || modalBVisible) {
+      const endpoint = modalAVisible ? "/contacts/" : "/country-contacts/US/";
+      const params = {
+        search: searchInput,
+        page,
+        page_size: pageSize,
+      };
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+      if (onlyEven) {
+        params.id__is_even = true;
+      }
+
+      axios
+        .get(`${apiBaseUrl}${endpoint}`, { params })
+        .then((response) => {
+          setContacts(response.data.results);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [modalAVisible, modalBVisible, searchInput, onlyEven, page]);
+
+  const openModalA = () => {
+    setModalAVisible(true);
+    setModalBVisible(false);
+    setPage(1);
+  };
+
+  const openModalB = () => {
+    setModalAVisible(false);
+    setModalBVisible(true);
+    setPage(1);
+  };
+
+  const openModalC = (contact) => {
+    setModalContact(contact);
+    setModalCVisible(true);
+  };
+
+  const closeModalC = () => {
+    setModalCVisible(false);
+  };
+
+  const handleSearchInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleOnlyEvenChange = (e) => {
+    setOnlyEven(e.target.checked);
+  };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+            onClick={openModalA}
+          >
+            All Contacts
+          </button>
+          <button
+            className="btn btn-lg btn-outline-warning"
+            type="button"
+            onClick={openModalB}
+          >
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Problem2;
+export default App;
